02fa8ecd8078157b5be0d6b66d150a4cbb1860b7
nicsrour <nicolas.e.srour@gmail.com>
2024-02-15 19:50:48 -0300

    third commit

--- first_django_project/blog/views.py
+++ first_django_project/blog/views.py
@@ -1,23 +1,11 @@
 from django.shortcuts import render
 
-posts = [
-    {
-        'author':'Nicolas S',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': '13 February, 2024'
-    },
-    {
-        'author':'Nicolas S',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': '14 February, 2024'
-    },
-]
+from .models import Post
+
 
 def home(request):
     context={
-        'posts_context':posts
+        'posts_context':Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
